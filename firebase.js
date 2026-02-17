554dc03966361bef3f7333f50c19ec8af39ce3af
THONANGI SONJYOTHSANA <thonangisanju11@gmail.com>
2026-02-17 11:46:49 +0530

    Update firebase configuration values
    Updated firebase configuration with new values.

--- firebase.js
+++ firebase.js
@@ -1,4 +1,12 @@
 const firebaseConfig = {
+  apiKey: "AIzaSyA1B2C3D4",
+  authDomain: "teacher-attendance.firebaseapp.com",
+  projectId: "teacher-attendance",
+  storageBucket: "teacher-attendance.appspot.com",
+  messagingSenderId: "987654321",
+  appId: "1:987654321:web:abcd1234"
+};
+const firebaseConfig = {
   apiKey: "PASTE_FROM_FIREBASE",
   authDomain: "PASTE_FROM_FIREBASE",
   projectId: "PASTE_FROM_FIREBASE",
