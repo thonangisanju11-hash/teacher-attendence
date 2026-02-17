2810941980a51efd761176535e7bbae1e1fcb395
THONANGI SONJYOTHSANA <thonangisanju11@gmail.com>
2026-02-17 11:44:21 +0530

    Update Firebase configuration values
    Updated Firebase configuration with new values.

--- firebase.js
+++ firebase.js
@@ -1,9 +1,13 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT"
+  apiKey: "PASTE_FROM_FIREBASE",
+  authDomain: "PASTE_FROM_FIREBASE",
+  projectId: "PASTE_FROM_FIREBASE",
+  storageBucket: "PASTE_FROM_FIREBASE",
+  messagingSenderId: "PASTE_FROM_FIREBASE",
+  appId: "PASTE_FROM_FIREBASE"
 };
 
 firebase.initializeApp(firebaseConfig);
+
 const auth = firebase.auth();
 const db = firebase.firestore();
