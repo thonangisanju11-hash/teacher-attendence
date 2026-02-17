6ae994334ab0161b0cd214a9c0d5c4044b9af2c7
THONANGI SONJYOTHSANA <thonangisanju11@gmail.com>
2026-02-17 10:44:14 +0530

    Initialize Firebase with configuration settings

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
