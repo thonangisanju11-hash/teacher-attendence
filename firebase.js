71c461e5a7b92c33c542647115d821d6ec1dae9b
THONANGI SONJYOTHSANA <thonangisanju11@gmail.com>
2026-02-17 11:48:31 +0530

    Update Firebase API key in configuration

--- firebase.js
+++ firebase.js
@@ -1,21 +1,18 @@
+// firebase.js
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA1B2C3D4",
+  apiKey: "AIzaSyXXXXXXX",
   authDomain: "teacher-attendance.firebaseapp.com",
   projectId: "teacher-attendance",
   storageBucket: "teacher-attendance.appspot.com",
   messagingSenderId: "987654321",
   appId: "1:987654321:web:abcd1234"
 };
-const firebaseConfig = {
-  apiKey: "PASTE_FROM_FIREBASE",
-  authDomain: "PASTE_FROM_FIREBASE",
-  projectId: "PASTE_FROM_FIREBASE",
-  storageBucket: "PASTE_FROM_FIREBASE",
-  messagingSenderId: "PASTE_FROM_FIREBASE",
-  appId: "PASTE_FROM_FIREBASE"
-};
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// Services
 const auth = firebase.auth();
 const db = firebase.firestore();
+
